9ac1162f54587795039b350cc8104dd6ce16862e
Israel-Ferreira <israel.souza5@fatec.sp.gov.br>
2018-11-12 17:24:39 -0200

    OK

--- home.php
+++ home.php
@@ -19,15 +19,18 @@
 
       <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
         <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-          <li class="nav-item active">
-            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Contatos</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Sugestões</a>
-          </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Contatos</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Sugestões</a>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link">Configurações</a>
+            </li>
         </ul>
         <form class="form-inline my-2 my-lg-1">
           <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
@@ -36,7 +39,6 @@
             <button type="submit" class="btn btn-outline-danger my-2 my-sm-0 ">Sair</button>
           </div>
         </form>
-        
       </div>
     </nav>
   </header>
@@ -45,5 +47,6 @@
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  
 </footer>
 </html>
