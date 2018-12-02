b0e2778cf5ef9f27ac21f13037c3e65732083b02
Israel-Ferreira <israel.souza5@fatec.sp.gov.br>
2018-12-02 20:03:39 -0200

    A parte do login está pronta

--- login.php
+++ login.php
@@ -1,25 +1,24 @@
 <?php
-require_once './db/Db_Conn.php';
-require_once './db/DbFunc.php';
+require_once 'db/Db_Conn.php';
+require_once 'db/DbFunc.php';
+require_once 'helpers/help_func.php';
 
-$db = new DbConn();
-$conn = $db->connect("linkedin_musical");
 
 session_start();
 
 
 if(verifica_campo('email') && verifica_campo('senha')){
-$email = $_POST['email'];
-$senha =  $_POST['senha'];
-
-
-if($conn->connect_errno){
-    die("Falha ao conectar com o banco de dados");
-    header("location:index.html");
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
+}else{
+    header('Location:index.html');
 }
 
-$orm = new DbFunc($conn); //"SELECT user_Email,senha FROM cantor WHERE user_Email = '$email' AND senha = $senha"
-$resultados =  $orm->procurar_usuario("cantor",$email,$senha);
+echo "<script>alert($email);</script>";
+
+
+
+$resultados =  DbFunc::procurar_usuario("cantor",$email,$senha,$conexao);
 if(mysqli_num_rows($resultados) <= 0 && mysqli_num_rows($resultados) > 1){
     header("Location:index.html");
 }else{
